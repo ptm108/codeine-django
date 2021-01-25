e9bf1c52c0a43b71a31b98387c7b6ac4578f30a0
Tze Ming <e0311042@u.nus.edu>
2021-01-25 20:36:22 +0800

    Fix typo

--- codeine_django/common/urls.py
+++ codeine_django/common/urls.py
@@ -4,5 +4,5 @@ from . import views_member
 
 urlpatterns = [
   path('createMember', views_member.create_member, name='Create Member'),
-  path('members/<slug:pk>', siews_member.single_member_view, name='RUD for members'),
+  path('members/<slug:pk>', views_member.single_member_view, name='RUD for members'),
 ]
