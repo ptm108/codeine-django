b73aa180302cee621b3224670513af5757c1fe0f
Tze Ming <e0311042@u.nus.edu>
2021-01-25 19:28:43 +0800

    Init user models

--- codeine_django/common/models.py
+++ codeine_django/common/models.py
@@ -34,14 +34,11 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_admin', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('is_staff', True)
 
         if extra_fields.get('is_admin') is not True:
             raise ValueError(_('Superuser must have is_admin=True.'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
         # end ifs 
 
         return self.create_user(email, password, **extra_fields)
@@ -53,7 +50,6 @@ class BaseUser(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     email = models.EmailField(_('email address'), unique=True)
     is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
