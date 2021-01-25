fd53b564f210f74d649d0ed8bd3112e14bf0297c
Tze Ming <e0311042@u.nus.edu>
2021-01-25 17:31:39 +0800

    Add drf endpoints

--- codeine_django/codeine_django/urls.py
+++ codeine_django/codeine_django/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+
+    # auth end points
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
