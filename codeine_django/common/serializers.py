9dad3fd4472a23bbed3d721a3c45b07dc17333a5
Tze Ming <e0311042@u.nus.edu>
2021-01-27 16:30:30 +0800

    Update base user serializer

--- codeine_django/common/serializers.py
+++ codeine_django/common/serializers.py
@@ -4,10 +4,18 @@ from .models import Member, BaseUser
 
 
 class BaseUserSerializer(serializers.ModelSerializer):
+    profile_photo = serializers.SerializerMethodField('get_profile_photo_url')
     class Meta:
         model = BaseUser
-        fields = ('email', 'is_active', 'date_joined')
+        fields = ('email', 'is_active', 'date_joined', 'profile_photo')
     # end Meta
+
+    def get_profile_photo_url(self, obj):
+        request = self.context.get("request")
+        if obj.profile_photo and hasattr(obj.profile_photo, 'url'):
+            return request.build_absolute_uri(obj.profile_photo.url)
+        # end if
+    # end def
 # end class
 
 class MemberSerializer(serializers.ModelSerializer):
