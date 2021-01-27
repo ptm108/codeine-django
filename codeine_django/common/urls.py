cf868fb259677d1b5e7ad9945fcaafb6c5d94ef4
Tze Ming <e0311042@u.nus.edu>
2021-01-27 12:25:33 +0800

    Update member endpoints

--- codeine_django/common/urls.py
+++ codeine_django/common/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views_member
 
 urlpatterns = [
-  path('createMember', views_member.create_member, name='Create Member'),
+  path('members', views_member.create_member, name='Create/Get all/Search Member'),
   path('members/<slug:pk>', views_member.single_member_view, name='RUD for members'),
 ]
--- codeine_django/common/views_member.py
+++ codeine_django/common/views_member.py
@@ -1,6 +1,7 @@
 from django.db import transaction
 from django.db.utils import IntegrityError
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.db.models import Q
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes, parser_classes, renderer_classes
 from rest_framework.response import Response
@@ -14,7 +15,7 @@ from .models import BaseUser, Member
 from .serializers import MemberSerializer
 
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 @permission_classes((AllowAny,))
 def create_member(request):
     '''
@@ -39,11 +40,33 @@ def create_member(request):
                 return Response(status=status.HTTP_400_BAD_REQUEST)
         # end with
     # end if
+
+    '''
+    Get all members
+    '''
+    if request.method == 'GET':
+        # extract query params
+        search = request.query_params.get('search', None)
+
+        members = Member.objects
+
+        if search is not None:
+            members = members.filter(
+                Q(first_name__icontains=search) |
+                Q(last_name__icontains=search) |
+                Q(user__email__icontains=search)
+            )
+        # end if
+
+        serializer = MemberSerializer(members.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    # end if
 # end def
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes((IsAuthenticatedOrReadOnly,))
-def single_member_view(request, pk): 
+def single_member_view(request, pk):
     '''
     Gets a member by primary key/ id
     '''
@@ -52,7 +75,7 @@ def single_member_view(request, pk):
             member = Member.objects.get(pk=pk)
 
             return Response(MemberSerializer(member).data)
-        except (ObjectDoesNotExist, KeyError, ValueError) as e: 
+        except (ObjectDoesNotExist, KeyError, ValueError) as e:
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
     # end if
