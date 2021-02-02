3ad31c00bbb8713803625402db23c83073ebcc86
Tze Ming <e0311042@u.nus.edu>
2021-02-02 23:47:32 +0800

    Formatting

--- codeine_django/codeine_django/urls.py
+++ codeine_django/codeine_django/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
 
     # common infra endpoints
     path('auth/', include('common.urls'), name='Common infra end points')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
