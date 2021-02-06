ddbec559e9aa8e51181c78a82615830e27206f95
Tze Ming <e0311042@u.nus.edu>
2021-02-06 23:15:43 +0800

    Add delete course views

--- codeine_django/courses/views_course.py
+++ codeine_django/courses/views_course.py
@@ -28,49 +28,53 @@ def course_view(request):
     Params: search, sort
     '''
     if request.method == 'GET':
-        # extract query params
-        search = request.query_params.get('search', None)
-        date_sort = request.query_params.get('sortDate', None)
-        price_sort = request.query_params.get('sortPrice', None)
-        rating_sort = request.query_params.get('sortRating', None)
+        try:
+            # extract query params
+            search = request.query_params.get('search', None)
+            date_sort = request.query_params.get('sortDate', None)
+            price_sort = request.query_params.get('sortPrice', None)
+            rating_sort = request.query_params.get('sortRating', None)
 
-        # get pagination params from request, default is (10, 1)
-        page_size = int(request.query_params.get('pagesize', 10))
+            # get pagination params from request, default is (10, 1)
+            page_size = int(request.query_params.get('pagesize', 10))
 
-        courses = Course.objects
+            courses = Course.objects
 
-        if search is not None:
-            courses = courses.filter(
-                Q(title__icontains=search) |
-                Q(description__icontains=search) |
-                Q(coding_languages__icontains=search) |
-                Q(categories__icontains=search) |
-                Q(content_provider__company_name__icontains=search) |
-                Q(content_provider__user__first_name__icontains=search) |
-                Q(content_provider__user__last_name__icontains=search)
-            )
-        # end if
+            if search is not None:
+                courses = courses.filter(
+                    Q(title__icontains=search) |
+                    Q(description__icontains=search) |
+                    Q(coding_languages__icontains=search) |
+                    Q(categories__icontains=search) |
+                    Q(content_provider__company_name__icontains=search) |
+                    Q(content_provider__user__first_name__icontains=search) |
+                    Q(content_provider__user__last_name__icontains=search)
+                )
+            # end if
 
-        if date_sort is not None:
-            courses = courses.order_by(date_sort)
-        # end if
+            if date_sort is not None:
+                courses = courses.order_by(date_sort)
+            # end if
 
-        if price_sort is not None:
-            courses = courses.order_by(price_sort)
-        # end if
+            if price_sort is not None:
+                courses = courses.order_by(price_sort)
+            # end if
 
-        if rating_sort is not None:
-            courses = courses.order_by(rating_sort)
-        # end if
+            if rating_sort is not None:
+                courses = courses.order_by(rating_sort)
+            # end if
 
-        # paginator configs
-        paginator = PageNumberPagination()
-        paginator.page_size = page_size
+            # paginator configs
+            paginator = PageNumberPagination()
+            paginator.page_size = page_size
 
-        result_page = paginator.paginate_queryset(courses.all(), request)
-        serializer = CourseSerializer(result_page, many=True, context={"request": request})
+            result_page = paginator.paginate_queryset(courses.all(), request)
+            serializer = CourseSerializer(result_page, many=True, context={"request": request})
 
-        return paginator.get_paginated_response(serializer.data)
+            return paginator.get_paginated_response(serializer.data)
+        except (ValueError) as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        # end try-except
     # end if
 
     '''
@@ -95,16 +99,19 @@ def course_view(request):
                 languages=json.loads(data['languages']),
                 categories=json.loads(data['categories']),
                 price=data['price'],
+                exp_points=data['exp_points'],
                 content_provider=content_provider
             )
             course.save()
 
             return Response(CourseSerializer(course, context={'request': request}).data, status=status.HTTP_200_OK)
-        except (ObjectDoesNotExist, KeyError) as e:
+        except (KeyError, TypeError, ValueError) as e:
+            print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
         # end try-except
     # end if
 # end def
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes((IsAuthenticatedOrReadOnly,))
@@ -137,24 +144,48 @@ def single_course_view(request, pk):
             # end if
 
             data = request.data
-            
-            course.title=data['title']
-            course.learning_objectives=json.loads(data['learning_objectives'])
-            course.requirements=json.loads(data['requirements'])
-            course.description=data['description']
-            course.introduction_video_url=data['introduction_video_url']
-            course.thumbnail=data['thumbnail']
-            course.coding_languages=json.loads(data['coding_languages'])
-            course.languages=json.loads(data['languages'])
-            course.categories=json.loads(data['categories'])
-            course.price=data['price']
-            course.save() # save course
+
+            course.title = data['title']
+            course.learning_objectives = json.loads(data['learning_objectives'])
+            course.requirements = json.loads(data['requirements'])
+            course.description = data['description']
+            course.introduction_video_url = data['introduction_video_url']
+            course.thumbnail = data['thumbnail']
+            course.coding_languages = json.loads(data['coding_languages'])
+            course.languages = json.loads(data['languages'])
+            course.categories = json.loads(data['categories'])
+            course.price = data['price']
+            course.exp_points = int(data['exp_points'])
+            course.save()  # save course
 
             return Response(CourseSerializer(course, context={'request': request}).data, status=status.HTTP_200_OK)
-        except KeyError:
+        except (KeyError, TypeError, ValueError) as e:
+            print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        except ObjectDoesNotExist: 
+        except ObjectDoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         # end try-except
-    # end if 
+    # end if
+
+    '''
+    Delete a course
+    '''
+    if request.method == 'DELETE':
+        try:
+            user = request.user
+            content_provider = ContentProvider.objects.get(user=user)
+
+            course = Course.objects.get(pk=pk)
+
+            # check if content provider is owner of course
+            if course.content_provider != content_provider:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            # end if
+
+            course.is_deleted = True
+            return Response(CourseSerializer(course, context={'request': request}).data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        # end try-except
+    # end if
 # end def
