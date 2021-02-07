2e3393364861f61481f3ca519d8feca41a50a17d
Tze Ming <e0311042@u.nus.edu>
2021-02-08 00:30:28 +0800

    Add create chapter

--- codeine_django/courses/views_chapters.py
+++ codeine_django/courses/views_chapters.py
@@ -11,9 +11,12 @@ from rest_framework.permissions import (
 from .models import Course, Chapter
 from .serializers import ChapterSerializer
 
+from common.models import ContentProvider
+from common.permissions import IsContentProviderOnly, IsContentProviderOrReadOnly
+
 
 @api_view(['GET', 'POST'])
-@permission_classes((IsAuthenticated,))
+@permission_classes((IsContentProviderOnly,))
 def chapter_views(request, pk):
     '''
     Get all chapters under Course(pk=pk)
@@ -30,6 +33,43 @@ def chapter_views(request, pk):
             return Response(status=status.HTTP_400_BAD_REQUEST)
         except ObjectDoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
+        # end try-except
+    # end if
+
+    '''
+    Creates a new chapter 
+    Adds chapter to course(pk=pk)
+    '''
+    if request.method == 'POST':
+        try:
+            user = request.user
+            content_provider = ContentProvider.objects.get(user=user)
+
+            course = Course.objects.get(pk=pk)
+
+            # check if content provider is owner of course
+            if course.content_provider != content_provider:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            # end if
+
+            data = request.data
+
+            chapter = Chapter(
+                title=data['title'],
+                description=data['overview'],
+                order=data['order'],
+                course=course
+            )
+            chapter.save()
+
+            serializer = ChapterSerializer(chapter, context={'public': True})
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except (ValueError, KeyError) as e:
+            print(e)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         # end try-except
     # end if
 # end def
