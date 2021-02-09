d497c31ef52b2dac2b7148e5e9643d3b2899171c
Tze Ming <e0311042@u.nus.edu>
2021-02-09 16:20:15 +0800

    Refractor permissions

--- codeine_django/common/permissions.py
+++ codeine_django/common/permissions.py
@@ -1,36 +1,35 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsContentProviderOnly(BasePermission):
+class IsPartnerOnly(BasePermission):
     '''
-    View level check if requesting user is a ContentProvider
+    View level check if requesting user is a Partner
     '''
 
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
         # end if
-        return hasattr(request.user, 'content_provider')
+        return hasattr(request.user, 'partner')
     # end def
+
 # end class
 
-
-class IsContentProviderOrReadOnly(BasePermission):
+class IsPartnerOrReadOnly(BasePermission):
     '''
     View level check for unsafe methods
-    Check if requesting user is a ContentProvider
+    Check if requesting user is a Partner
     '''
 
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        if request.user.is_authenticated and hasattr(request.user, 'content_provider'):
+        if request.user.is_authenticated and hasattr(request.user, 'partner'):
             return True
         # end ifs
         return False
     # end def
 # end class
-
 
 class IsMemberOnly(BasePermission):
     '''
@@ -57,37 +56,6 @@ class IsMemberOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         if request.user.is_authenticated and hasattr(request.user, 'member'):
-            return True
-        # end ifs
-        return False
-    # end def
-# end class
-
-
-class IsIndustryPartnerOnly(BasePermission):
-    '''
-    View level check if requesting user is a IndustryPartner
-    '''
-
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        # end if
-        return hasattr(request.user, 'industry_partner')
-    # end def
-# end class
-
-
-class IsIndustryPartnerOrReadOnly(BasePermission):
-    '''
-    View level check for unsafe methods
-    Check if requesting user is a IndustryPartner
-    '''
-
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        if request.user.is_authenticated and hasattr(request.user, 'industry_partner'):
             return True
         # end ifs
         return False
