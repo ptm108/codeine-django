9a6554f5c66f47c5facb82c8893151a8df8cffde
Tze Ming <e0311042@u.nus.edu>
2021-02-09 21:11:29 +0800

    update custom jwt endpoint

--- codeine_django/codeine_django/urls.py
+++ codeine_django/codeine_django/urls.py
@@ -19,14 +19,14 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views_auth import CustomTokenObtainPairView
+from .views_auth import authenticate_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 
     # auth end points
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', authenticate_user, name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # common infra endpoints
--- codeine_django/codeine_django/views_auth.py
+++ codeine_django/codeine_django/views_auth.py
@@ -1,22 +1,33 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from common.serializers import NestedBaseUserSerializer
 
 
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def authenticate_user(request):
+    '''
+    Authenticate user credentials
+    '''
+    if request.method == 'POST':
+        data = request.data
+        user = authenticate(**data)
 
-        # Add custom claims
-        token['user'] = NestedBaseUserSerializer(user).data
+        # get JWT
+        token = RefreshToken.for_user(user)
 
-        return token
-    # end def
-# end class
+        payload = {
+            'refresh': str(token),
+            'access': str(token.access_token),
+            'user': NestedBaseUserSerializer(user, context={'request': request}).data
+        }
 
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
-# end class
+        return Response(payload, status=status.HTTP_200_OK)
+    # end if
+# end def
