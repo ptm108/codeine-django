1d983fe4a0cf8c71d97c5c8887ad84c34c08cc70
Tze Ming <e0311042@u.nus.edu>
2021-02-09 21:01:58 +0800

    Add update org endpoint

--- codeine_django/common/urls.py
+++ codeine_django/common/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views_member, views_partners
+from . import views_member, views_partners, views_organization
 
 urlpatterns = [
     # members views
@@ -14,4 +14,7 @@ urlpatterns = [
     path('partners/<slug:pk>', views_partners.single_partner_view, name='Read/update/delete for partners'),
     path('partners/<slug:pk>/changePassword', views_partners.partner_change_password_view, name='Partner change password'),
     path('partners/<slug:pk>/activate', views_partners.activate_partner_view, name='Activates partner'),
+
+    # update organization
+    path('organizations/<slug:pk>', views_organization.single_organization_view, name='Update organization detail'),
 ]
---
+++ codeine_django/common/views_organization.py
@@ -0,0 +1,37 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
+
+from .models import Organization, Partner
+from .serializers import OrganizationSerializer
+from .permissions import IsPartnerOrAdminOrReadOnly
+
+
+@api_view(['PUT'])
+@permission_classes((IsPartnerOrAdminOrReadOnly,))
+@parser_classes((MultiPartParser, FormParser))
+def single_organization_view(request, pk):
+    '''
+    Updates organization details
+    '''
+    if request.method == 'PUT':
+        try:
+            user = request.user
+            partner = Partner.objects.get(user=user)
+
+            organization = Organization.objects.filter(partners__contains=partner).get(pk=pk)
+
+            if 'organization_name' in data:
+                user.organization_name = data['organization_name']
+            if 'organization_photo' in data:
+                user.organization_photo = data['organization_photo']
+            # end ifs
+            organization.save()
+
+            return Response(OrganizationSerializer(organization, context={'request': request}).data, status=status.HTTP_200_OK)
+        except Organization.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        # end try-except
+    # end if
+# end def
