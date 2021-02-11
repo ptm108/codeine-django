3642cc1973934d603c6de5383724a08704ee3d74
Tze Ming <e0311042@u.nus.edu>
2021-02-11 16:45:00 +0800

    Refractor changes

--- codeine_django/courses/urls_course_materials.py
+++ codeine_django/courses/urls_course_materials.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # course material views
     path('/<slug:material_id>/files', views_course_materials.update_file_view, name='Update material (file)'),
     path('/<slug:material_id>/videos', views_course_materials.update_video_view, name='Update material (video)'),
-    path('/<slug:material_id>', views_course_materials.delete_material_view, name='Delete Material'),
+    path('/<slug:material_id>', views_course_materials.single_material_view, name='Get/Delete Material'),
 ]
