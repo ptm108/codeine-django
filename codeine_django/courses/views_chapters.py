dda3b5bfd80ce932a184b76108f162d4a903d7fa
Tze Ming <e0311042@u.nus.edu>
2021-02-11 14:46:35 +0800

    Code cleanup

--- codeine_django/courses/views_chapters.py
+++ codeine_django/courses/views_chapters.py
@@ -2,7 +2,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
 
 from .models import Course, Chapter
 from .serializers import CourseSerializer, ChapterSerializer
