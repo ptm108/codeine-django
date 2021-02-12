ce6c982d9510b6914c27160850b44717505dfb66
eliz <e0319168@u.nus.edu>
2021-02-12 17:53:28 +0800

    serializers for achievements and requirements

--- codeine_django/achievements/serializer.py
+++ codeine_django/achievements/serializer.py
@@ -1,1 +1,29 @@
 from rest_framework import serializers
+from .models import (
+    Achievement, 
+    AchievementRequirement
+)
+
+class AchievementRequirementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AchievementRequirement
+        fields = '__all__'
+    # end Meta
+# end class
+
+class AchievementSerializer(serializers.ModelSerializer):
+    requirements = AchievementRequirementSerializer(many=True)
+    badge = serializers.SerializerMethodField('get_badge_url')
+
+    class Meta:
+        model = Achievement
+        fields = '__all__'
+    # end Meta
+
+    def get_badge_url(self, obj):
+        request = self.context.get("request")
+        if obj.badge and hasattr(obj.badge, 'url'):
+            return request.build_absolute_uri(obj.badge.url)
+        # end if
+    # end def
+# end class
