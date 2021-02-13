a3495b8de39d41c063b92bef59e3669ff0f1f04d
eliz <e0319168@u.nus.edu>
2021-02-13 23:17:17 +0800

    update achievement endpoint

--- codeine_django/achievements/views.py
+++ codeine_django/achievements/views.py
@@ -1,6 +1,7 @@
 from .models import Achievement
 from .serializer import AchievementSerializer
 from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
@@ -76,6 +77,30 @@ def single_achievement_view(request, pk):
             serializer = AchievementSerializer(achievement, context={"request": request})
             return Response(serializer.data, status=status.HTTP_200_OK)
         except (ValueError) as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        # end try-except
+    # end if
+
+    '''
+    Update an Achievement
+    '''
+    if request.method == 'PATCH':
+        try:
+            achievement = Achievement.objects.get(pk=pk)
+            data = request.data
+
+            if 'title' in data:
+                achievement.title = data['title']
+            if 'badge' in data:
+                achievement.badge=data['badge']
+
+            achievement.save() 
+
+            serializer = AchievementSerializer(achievement, context={"request": request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except (KeyError, ValueError) as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         # end try-except
     # end if
