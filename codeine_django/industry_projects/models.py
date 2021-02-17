5b48ef150ac4053418b328003c18002015810b0c
eliz <e0319168@u.nus.edu>
2021-02-18 00:35:02 +0800

    create IndustryProject and application models

--- codeine_django/industry_projects/models.py
+++ codeine_django/industry_projects/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+import uuid
+
+class IndustryProject(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    start_date = models.DateField(null=True, default=None)
+    end_date = models.DateField(null=True, default=None)
+    application_deadline = models.DateField(null=True, default=None)
+    is_available = models.BooleanField(default=True)
+    is_completed = models.BooleanField(default=False)
+
+    # provider ref
+    partner = models.ForeignKey('common.Partner', on_delete=models.SET_NULL, related_name='industry_projects', null=True)
+
+# end class
+
+class IndustryProjectApplication(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    is_accepted = models.BooleanField(default=False)
+
+    # member ref
+    member = models.ForeignKey('common.Member', on_delete=models.SET_NULL, related_name='industry_project_applications', null=True)
+
+# end class
