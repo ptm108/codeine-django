bab284e6f258617ae28e66b774d1ee1d3b0f778a
dtjldamien <dtjldamien@gmail.com>
2021-02-17 16:22:49 +0800

    code review comments API

--- codeine_django/community/urls_articles.py
+++ codeine_django/community/urls_articles.py
@@ -9,5 +9,5 @@ urlpatterns = [
 
     # article comment views
     path('/<slug:article_id>/comments', views_article_comments.article_comment_view, name='Create/Get all/Search Article Comments'),
-    path('/<slug:article_id>/comments/<slug:pk>', views_article_comments.single_article_comment_view, name='Create/Get all/Search Article Comments'),
+    path('/<slug:article_id>/comments/<slug:pk>', views_article_comments.single_article_comment_view, name='Read/update/delete for Article Comments'),
 ]
--- codeine_django/community/urls_code_reviews.py
+++ codeine_django/community/urls_code_reviews.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from . import views_code_review
+from . import views_code_review, views_code_review_comments
 
 urlpatterns = [
     # code review views
     path('', views_code_review.code_review_view, name='Create/Get all/Search Code Reviews'),
-    path('/<slug:pk>', views_code_review.single_code_review_view, name='Read/update/delete for Code Review')
+    path('/<slug:pk>', views_code_review.single_code_review_view, name='Read/update/delete for Code Review'),
+
+    # code review comment views
+    path('/<slug:code_review_id>/comments', views_code_review_comments.code_review_comment_view, name='Create/Get all/Search Code Review Comments'),
+    path('/<slug:code_review_id>/comments/<slug:pk>', views_code_review_comments.single_code_review_comment_view, name='Read/update/delete Code Review Comments'),
 ]
