b12d18f6c29869603dd7743343899996fa223099
eliz <e0319168@u.nus.edu>
2021-02-18 17:29:04 +0800

    update IP Application serializer

--- codeine_django/industryProjects/serializers.py
+++ codeine_django/industryProjects/serializers.py
@@ -6,10 +6,17 @@ from .models import (
 )
 
 class IndustryProjectApplicationSerializer(serializers.ModelSerializer):
+    member = serializers.SerializerMethodField('get_base_user')
+    
     class Meta:
         model = IndustryProjectApplication
         fields = '__all__'
     # end Meta
+
+    def get_base_user(self, obj):
+        request = self.context.get("request")
+        return NestedBaseUserSerializer(obj.member.user, context={'request': request}).data
+    # end def
 # end class
 
 class IndustryProjectSerializer(serializers.ModelSerializer):
