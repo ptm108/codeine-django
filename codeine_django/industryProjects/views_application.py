b91a1e9617cec7d63be4c6fd0ce498f0e56fc693
eliz <e0319168@u.nus.edu>
2021-02-18 18:02:53 +0800

    Read/Accept/Delete Application endpoints

--- codeine_django/industryProjects/views_application.py
+++ codeine_django/industryProjects/views_application.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from common.models import Member
-from common.permissions import IsPartnerOnly, IsMemberOrReadOnly
+from common.permissions import IsPartnerOrReadOnly, IsMemberOrReadOnly
 
 @api_view(['GET', 'POST'])
 @permission_classes((IsMemberOrReadOnly,))
@@ -52,4 +52,75 @@ def application_view(request, pk):
     # end if
 # end def
 
+@api_view(['GET', 'POST', 'DELETE'])
+@permission_classes((IsPartnerOrReadOnly,))
+def single_application_view(request, pk, app_id):
 
+    '''
+    Get Application by ID
+    '''
+    if request.method == 'GET':
+        try:
+            application = IndustryProjectApplication.objects.get(pk=app_id)
+
+            serializer = IndustryProjectApplicationSerializer(application, context={"request": request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except (ValueError) as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)    
+        # end try-except
+    # end if
+
+    '''
+    Partner accepts Member / Update Application
+    '''
+    if request.method == 'POST':
+        try:
+            application = IndustryProjectApplication.objects.get(pk=app_id)
+            application.is_accepted = True
+                        
+            application.save() 
+
+            serializer = IndustryProjectApplicationSerializer(application, context={"request": request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except (KeyError, ValueError) as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        # end try-except
+    # end if
+
+    '''
+    Delete an Application
+    '''
+    if request.method == 'DELETE':
+        try:
+            application = IndustryProjectApplication.objects.get(pk=app_id)
+            application.delete()
+
+            return Response(status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        # end try-except
+    # end if
+# end def
+
+@api_view(['DELETE'])
+@permission_classes((IsMemberOrReadOnly,))
+def delete_application_view(request, pk, app_id):
+
+    '''
+    Delete an Application
+    '''
+    if request.method == 'DELETE':
+        try:
+            application = IndustryProjectApplication.objects.get(pk=app_id)
+            application.delete()
+
+            return Response(status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        # end try-except
+    # end if
+# end def
