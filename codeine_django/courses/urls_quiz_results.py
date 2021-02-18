30aec75e7f65ca53d2384083cf3ae3a2b6a785b6
Tze Ming <e0311042@u.nus.edu>
2021-02-18 18:32:07 +0800

    Submit quiz view

--- codeine_django/courses/urls_quiz_results.py
+++ codeine_django/courses/urls_quiz_results.py
@@ -4,5 +4,6 @@ from . import views_quiz_result
 
 urlpatterns = [
     # Quiz views
-    path('/<slug:quiz_result_id>', views_quiz_result.update_quiz_result_view, name='Update quiz responses, Submit quiz'),
+    path('/<slug:quiz_result_id>', views_quiz_result.update_quiz_result_view, name='Update quiz response'),
+    path('/<slug:quiz_result_id>/submit', views_quiz_result.sumbit_quiz_result_view, name='Submit quiz'),
 ]
