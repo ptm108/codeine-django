7a4c5affca5aac92e2cba90c6fe65109c4ed7bb3
Damien <dtjldamien@gmail.com>
2021-02-19 15:39:11 +0800

    Update serializers.py
    to return member and partner objects

--- codeine_django/consultations/serializers.py
+++ codeine_django/consultations/serializers.py
@@ -1,13 +1,30 @@
 from rest_framework import serializers
 
 from .models import ConsultationSlot, PaymentTransaction
-
+from common.serializers import NestedBaseUserSerializer
 
 class ConsultationSlotSerializer(serializers.ModelSerializer):
+    member = serializers.SerializerMethodField('get_member_base_user')
+    partner = serializers.SerializerMethodField('get_partner_base_user')
+
     class Meta:
         model = ConsultationSlot
         fields = '__all__'
     # end Meta
+
+    def get_member_base_user(self, obj):
+        if obj.member is None:
+            return None
+        else:
+            request = self.context.get("request")
+            return NestedBaseUserSerializer(obj.member.user, context={'request': request}).data
+        # end if-else
+    # end def
+
+    def get_partner_base_user(self, obj):
+        request = self.context.get("request")
+        return NestedBaseUserSerializer(obj.partner.user, context={'request': request}).data
+    # end def
 # end class
 
 class PaymentTransactionSerializer(serializers.ModelSerializer):
