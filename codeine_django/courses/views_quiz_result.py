a81bd41390226b810856766e792b7ebc58a58566
Tze Ming <e0311042@u.nus.edu>
2021-02-23 22:24:59 +0800

    Update progress upon assessment submission

--- codeine_django/courses/views_quiz_result.py
+++ codeine_django/courses/views_quiz_result.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.db import transaction
 from django.db.utils import IntegrityError
 from django.db.models import Q, Sum
 from rest_framework.decorators import api_view, permission_classes, parser_classes
@@ -119,51 +120,61 @@ def sumbit_quiz_result_view(request, quiz_result_id):
 
             quiz = quiz_result.quiz
 
-            total_marks = ShortAnswer.objects.filter(question__quiz=quiz).aggregate(Sum('marks'))['marks__sum'] if ShortAnswer.objects.filter(question__quiz=quiz).exists() else 0
-            total_marks += MRQ.objects.filter(question__quiz=quiz).aggregate(Sum('marks'))['marks__sum'] if MRQ.objects.filter(question__quiz=quiz).exists() else 0
-            total_marks += MCQ.objects.filter(question__quiz=quiz).aggregate(Sum('marks'))['marks__sum'] if MCQ.objects.filter(question__quiz=quiz).exists() else 0
-            score = 0
 
-            for answer in quiz_answers.all():
-                question = answer.question
+            with transaction.atomic():
+                total_marks = ShortAnswer.objects.filter(question__quiz=quiz).aggregate(Sum('marks'))['marks__sum'] if ShortAnswer.objects.filter(question__quiz=quiz).exists() else 0
+                total_marks += MRQ.objects.filter(question__quiz=quiz).aggregate(Sum('marks'))['marks__sum'] if MRQ.objects.filter(question__quiz=quiz).exists() else 0
+                total_marks += MCQ.objects.filter(question__quiz=quiz).aggregate(Sum('marks'))['marks__sum'] if MCQ.objects.filter(question__quiz=quiz).exists() else 0
+                score = 0
 
-                # evaluate short answer questions
-                try:
-                    keywords = question.shortanswer.keywords
-                    responses = answer.response.split(' ')
-                    responses = [response for response in responses if response in keywords]
+                for answer in quiz_answers.all():
+                    question = answer.question
 
-                    score += len(responses) / len(keywords) * question.shortanswer.marks
-                except ShortAnswer.DoesNotExist:
-                    pass
-                # end try-except
+                    # evaluate short answer questions
+                    try:
+                        keywords = question.shortanswer.keywords
+                        responses = answer.response.split(' ')
+                        responses = [response for response in responses if response in keywords]
 
-                # evaluate mcq
-                try:
-                    if answer.response == question.mcq.correct_answer:
-                        score += question.mcq.marks
-                    # end if
-                except MCQ.DoesNotExist:
-                    pass
-                # end try-except
+                        score += len(responses) / len(keywords) * question.shortanswer.marks
+                    except ShortAnswer.DoesNotExist:
+                        pass
+                    # end try-except
 
-                # evaluate mrq
-                try:
-                    correct_answer = question.mrq.correct_answer
-                    responses = [response for response in answer.responses if response in correct_answer]
+                    # evaluate mcq
+                    try:
+                        if answer.response == question.mcq.correct_answer:
+                            score += question.mcq.marks
+                        # end if
+                    except MCQ.DoesNotExist:
+                        pass
+                    # end try-except
 
-                    score += len(responses) / len(correct_answer) * question.mrq.marks
-                except MRQ.DoesNotExist:
-                    pass
-                # end try-except
-            # end for
+                    # evaluate mrq
+                    try:
+                        correct_answer = question.mrq.correct_answer
+                        responses = [response for response in answer.responses if response in correct_answer]
 
-            quiz_result.score = score
-            quiz_result.submitted = True
-            if score >= quiz.passing_marks:
-                quiz_result.passed = True
-            # end if
-            quiz_result.save()
+                        score += len(responses) / len(correct_answer) * question.mrq.marks
+                    except MRQ.DoesNotExist:
+                        pass
+                    # end try-except
+                # end for
+
+                quiz_result.score = score
+                quiz_result.submitted = True
+                if score >= quiz.passing_marks:
+                    quiz_result.passed = True
+                # end if
+                quiz_result.save()
+
+                if hasattr(quiz, 'course'):
+                    course = quiz.course
+                    enrollment = Enrollment.objects.filter(course=course).get(member=member)
+                    enrollment.progress = 100
+                    enrollment.save()
+                # end if
+            # end with
 
             return Response(QuizResultSerializer(quiz_result).data, status=status.HTTP_200_OK)
         except ObjectDoesNotExist as e:
