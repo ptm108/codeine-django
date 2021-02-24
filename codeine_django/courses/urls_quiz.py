10e254cdfff00b1c0089217737a1fab204599059
Tze Ming <e0311042@u.nus.edu>
2021-02-25 00:09:35 +0800

    Add get all quiz view

--- codeine_django/courses/urls_quiz.py
+++ codeine_django/courses/urls_quiz.py
@@ -4,6 +4,7 @@ from . import views_quiz, views_quiz_result
 
 urlpatterns = [
     # Quiz views
+    path('', views_quiz.all_quiz_view, name='Get all quizzes'),
     path('/<slug:quiz_id>', views_quiz.quiz_view, name='Get quiz by id'),
     path('/<slug:quiz_id>/questions', views_quiz.add_question_view, name='Add question'),
     path('/<slug:quiz_id>/questions/<slug:question_id>', views_quiz.single_question_view, name='Add question'),
