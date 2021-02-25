ad7dbd41c0ef6c4e6bfe09ddeb249d3128a2eb0f
Tze Ming <e0311042@u.nus.edu>
2021-02-25 19:03:38 +0800

    Update member stats on quizresult save

--- codeine_django/courses/signals.py
+++ codeine_django/courses/signals.py
@@ -2,7 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db.models import Avg
 
-from .models import CourseReview, Course
+from .models import CourseReview, Course, QuizResult
+from utils.member_utils import get_member_stats
 
 
 @receiver(post_save, sender=CourseReview)
@@ -14,11 +15,12 @@ def aggregate_ratings(sender, instance, **kwargs):
     course.save()
 # end def
 
-# @receiver(post_save, sender=CourseReview)
-# def aggregate_ratings(sender, instance, **kwargs):
-#     course = instance.course
-#     reviews = CourseReview.objects.filter(course=course)
 
-#     course.rating = reviews.aggregate(Avg('rating'))['rating__avg']
-#     course.save()
-# # end def
+@receiver(post_save, sender=QuizResult)
+def update_stats(sender, instance, **kwargs):
+    # update member stats
+    member = instance.member
+    member.stats = get_member_stats(member.user.id)
+    member.save()
+
+# end def
