7593805e4beb0db54d168d5b700d728dbf626f3e
Tze Ming <e0311042@u.nus.edu>
2021-02-25 20:21:22 +0800

    Add trigger to set member achievement

--- codeine_django/courses/signals.py
+++ codeine_django/courses/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 from django.db.models import Avg
 
 from .models import CourseReview, Course, QuizResult
+from achievements.models import Achievement, MemberAchievement
 from utils.member_utils import get_member_stats
 
 
@@ -20,7 +21,32 @@ def aggregate_ratings(sender, instance, **kwargs):
 def update_stats(sender, instance, **kwargs):
     # update member stats
     member = instance.member
-    member.stats = get_member_stats(member.user.id)
+    stats = get_member_stats(member.user.id)
+    member.stats = stats
     member.save()
 
+    # check for new achievements
+    non_achievements = Achievement.objects.exclude(members_achievements__member=member).all()
+
+    for non_achievement in non_achievements:
+        passed = True
+
+        # check through requirements
+        for requirement in non_achievement.achievement_requirements.all():
+            if requirement.coding_languages in stats and stats[requirement.coding_languages] < requirement.experience_point:
+                print(requirement)
+                passed = False
+                break
+            # end if
+            if requirement.category in stats and stats[requirement.category] < requirement.experience_point:
+                print(requirement)
+                passed = False
+                break
+            # end if
+        # end for
+
+        if passed:
+            MemberAchievement(achievement=non_achievement, member=member).save()
+        # end if
+    # end for
 # end def
