2cf66396503c10aa77a29479b6145996dc3ffe0b
dtjldamien <dtjldamien@gmail.com>
2021-02-26 12:21:53 +0800

    update article and codereview comments

--- codeine_django/community/serializers.py
+++ codeine_django/community/serializers.py
@@ -1,20 +1,79 @@
 from rest_framework import serializers
 
 from .models import Article, ArticleComment, Engagement, CodeReview, CodeReviewComment
+from common.serializers import NestedBaseUserSerializer
+
+class NestedCodeReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CodeReview
+        fields = '__all__'
+    # end Meta
+# end class
+
+
+class ParentArticleCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleComment
+        fields = ('id')
+    # end Meta
+# end class
+
+
+class ParentCodeReviewCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CodeReviewComment
+        fields = ('id')
+    # end Meta
+# end class
 
 
 class ArticleCommentSerializer(serializers.ModelSerializer):
+    user = NestedBaseUserSerializer()
+    code_review = NestedCodeReviewSerializer()
+    parent_comment = serializers.SerializerMethodField('get_parent_comment')
 
     class Meta:
         model = ArticleComment
         fields = '__all__'
+    # end Meta'
+
+    def get_parent_comment(self, obj):
+        request = self.context.get("request")
+        if obj.parent_comment is None:
+            return None
+        else :
+            return ParentArticleCommentSerializer(obj.parent_comment, context={'request': request}).data
+        # end if-else
+    # end def
+
+# end class
+
+
+class CodeReviewCommentSerializer(serializers.ModelSerializer):
+    user = NestedBaseUserSerializer()
+    code_review = NestedCodeReviewSerializer()
+    parent_comment = serializers.SerializerMethodField('get_parent_comment')
+
+    class Meta:
+        model = CodeReviewComment
+        fields = '__all__'
     # end Meta
+
+    def get_parent_comment(self, obj):
+        request = self.context.get("request")
+        if obj.parent_comment is None:
+            return None
+        else :
+            return ParentCodeReviewCommentSerializer(obj.parent_comment, context={'request': request}).data
+        # end if-else
+    # end def
 # end class
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     top_level_comments = serializers.SerializerMethodField('get_top_level_comments')
     engagements = serializers.SerializerMethodField('get_engagements')
+    
     class Meta:
         model = Article
         fields = '__all__'
@@ -36,16 +95,6 @@ class EngagementSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Engagement
-        fields = '__all__'
-    # end Meta
-# end class
-
-
-
-class CodeReviewCommentSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = CodeReviewComment
         fields = '__all__'
     # end Meta
 # end class
