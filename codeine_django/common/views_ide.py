0e3d7f76249c2b78734e8735995a5ad06ac30c10
Tze Ming <e0311042@u.nus.edu>
2021-03-08 21:56:04 +0800

    init views

--- codeine_django/common/views_ide.py
+++ codeine_django/common/views_ide.py
@@ -8,19 +8,39 @@ from rest_framework.permissions import (
 )
 
 import docker
+import os
+import pwd
+
+
+def get_username():
+    return pwd.getpwuid(os.getuid())[0]
+# end def
+
 
 @api_view(['GET'])
-@permission_classes(['AllowAny'])
+@permission_classes((IsAuthenticated,))
 def init_ide(request):
     '''
     Test ide
     '''
     if request.method == 'GET':
+        user = request.user
         try:
             client = docker.from_env()
-            client.containers.run('codercom/code-server')
-        except:
-            print('error')
+            container = client.containers.run(
+                'code-server',
+                detach=True,
+                environment=[f'DOCKER_USER={get_username()}', 'GIT_URL=http://github.com/ptm108/photo-journal-rn'],
+                user=f'{os.getuid()}:{os.getgid()}',
+                name=f'code-server-{user.id}',
+                ports={'8080/tcp': None},
+            )
+            container.reload()
+            print(container.attrs)
+            return Response()
+        except Exception as e:
+            print('error', e)
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         # end try except
     # end if
 # end def
