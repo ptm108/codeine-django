995c40e376182cbc81745d0cecfa9e72cdbfe72a
Tze Ming <e0311042@u.nus.edu>
2021-03-12 17:13:47 +0800

    fix model, add migration

--- codeine_django/analytics/models.py
+++ codeine_django/analytics/models.py
@@ -10,6 +10,6 @@ class EventLog(models.Model):
     user = models.ForeignKey('common.BaseUser', on_delete=models.SET_NULL, null=True, blank=True, default=None)
     course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, null=True, blank=True, default=None)
     course_material = models.ForeignKey('courses.CourseMaterial', on_delete=models.CASCADE, null=True, blank=True, default=None)
-    quiz = models.ForeignKey('course.Quiz', on_delete=models.CASCADE, null=True, blank=True, default=None)
+    quiz = models.ForeignKey('courses.Quiz', on_delete=models.CASCADE, null=True, blank=True, default=None)
     industry_project = models.ForeignKey('industry-projects.IndustryProject', on_delete=models.CASCADE, null=True, blank=True, default=None)
 # end class
