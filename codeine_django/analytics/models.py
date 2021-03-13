66f7c5507b4daa8686c478a1358c32597cf1b838
Tze Ming <e0311042@u.nus.edu>
2021-03-13 23:19:23 +0800

    add payload ref

--- codeine_django/analytics/models.py
+++ codeine_django/analytics/models.py
@@ -5,7 +5,20 @@ import uuid
 
 class EventLog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+
+    # payloads:
+    # 1. "course view", course 
+    # 2. "continue course", course, user
+    # 3. "pause course", course, user,
+    # 4. "continue course material", course material, user
+    # 5. "stop course material", course material, user
+    # 6. "start assessment", quiz, user 
+    # 7. "stop assessment", quiz, user
+    # 8. "search course", search string, user
+    # 9. "search industry project", search string, user
+    # 10. "view industry project", industry project, user
     payload = models.CharField(max_length=255)
+    
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey('common.BaseUser', on_delete=models.SET_NULL, null=True, blank=True, default=None)
     course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, null=True, blank=True, default=None)
