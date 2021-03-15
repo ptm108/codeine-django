747441a415f1751b828d1f5a678a6b535ee0a373
Tze Ming <e0311042@u.nus.edu>
2021-03-16 00:11:12 +0800

    add search_string to event log

--- codeine_django/analytics/models.py
+++ codeine_django/analytics/models.py
@@ -7,18 +7,18 @@ class EventLog(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
 
     # payloads:
-    # 1. "course view", course 
+    # 1. "course view", course
     # 2. "continue course", course, user
     # 3. "stop course", course, user,
     # 4. "continue course material", course material, user
     # 5. "stop course material", course material, user
-    # 6. "start assessment", quiz, user 
+    # 6. "start assessment", quiz, user
     # 7. "stop assessment", quiz, user
     # 8. "search course", search string, user
     # 9. "search industry project", search string, user
     # 10. "view industry project", industry project, user
     payload = models.CharField(max_length=255)
-    
+
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey('common.BaseUser', on_delete=models.SET_NULL, null=True, blank=True, default=None)
     course = models.ForeignKey('courses.Course', on_delete=models.CASCADE, null=True, blank=True, default=None)
@@ -26,6 +26,8 @@ class EventLog(models.Model):
     quiz = models.ForeignKey('courses.Quiz', on_delete=models.CASCADE, null=True, blank=True, default=None)
     industry_project = models.ForeignKey('industry_projects.IndustryProject', on_delete=models.CASCADE, null=True, blank=True, default=None)
     duration = models.PositiveBigIntegerField(null=True, blank=True, default=None)
+
+    search_string = models.CharField(max_length=255)
 
     class Meta:
         ordering = ['-timestamp']
