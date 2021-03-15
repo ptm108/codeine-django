557e3fc85eba45f8c981ea82856eb0ad1716c1a9
Tze Ming <e0311042@u.nus.edu>
2021-03-15 16:03:09 +0800

    rename view, reorg

--- codeine_django/analytics/urls.py
+++ codeine_django/analytics/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from . import views
+from . import views_course_analytics
 
 urlpatterns = [
     # analytics views
-    path('', views.post_log_view, name='Create Event Log'),
-    path('/course-conversion-rate', views.course_conversion_rate_view, name='Course conversion metrics'),
-    path('/course-material-time', views.course_material_average_time_view, name='Course material time metrics'),
-    path('/course-time', views.course_average_time_view, name='Course time metrics'),
-    path('/inactive-members', views.inactive_members_view, name='Get inactive members by course'),
+    path('', views_course_analytics.post_log_view, name='Create Event Log'),
+    path('/course-conversion-rate', views_course_analytics.course_conversion_rate_view, name='Course conversion metrics'),
+    path('/course-material-time', views_course_analytics.course_material_average_time_view, name='Course material time metrics'),
+    path('/course-time', views_course_analytics.course_average_time_view, name='Course time metrics'),
+    path('/inactive-members', views_course_analytics.inactive_members_view, name='Get inactive members by course'),
 ]
