9fb6d0afa4558f5f7e1eafea279350ef3398988d
Damien Tan <dtjldamien@gmail.com>
2021-03-18 14:52:48 +0800

    fix: refactor CodeReviewEngagement
    to work like CodeReviewCommentEngagement

--- codeine_django/community/models.py
+++ codeine_django/community/models.py
@@ -210,7 +210,7 @@ class CodeReviewComment(models.Model):
 class CodeReviewEngagement(models.Model):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    like = models.IntegerField()
+    # like = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     # ref
