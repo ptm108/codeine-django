c7370276730e73bab8d1d37385e8b5033a3fbabb
Damien Tan <dtjldamien@gmail.com>
2021-03-30 18:18:47 +0800

    feat: update notification schema

--- codeine_django/common/models.py
+++ codeine_django/common/models.py
@@ -250,6 +250,16 @@ class MembershipSubscription(models.Model):
 
 
 class Notification(models.Model):
+    NOTIFICATION_TYPES = (
+        ('COURSE', 'Course'),
+        ('ARTICLE', 'Article'),
+        ('CODE_REVIEW', 'Code Review'),
+        ('PAYMENT', 'Payment'),
+        ('CONSULTATION', 'Consultation'),
+        ('HELPDESK', 'Helpdesk'),
+        ('INDUSTRY_PROJECTS', 'Industry Project'),
+    )
+
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     title = models.CharField(max_length=255)
@@ -257,9 +267,15 @@ class Notification(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
 
+    # enums
+    notification_type = models.TextField(
+        choices=NOTIFICATION_TYPES)
+
     # ref
     receiver = models.ForeignKey(
-        BaseUser, related_name='notifications', on_delete=models.SET_NULL, blank=True, null=True)
+        BaseUser, related_name='received_notifications', on_delete=models.SET_NULL, blank=True, null=True)
+    sender = models.ForeignKey(
+        BaseUser, related_name='sent_notifications', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f'Notification {self.id}'
