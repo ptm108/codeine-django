2926ec36ecc533b0191d3559cd2b34905f3369d2
Damien Tan <dtjldamien@gmail.com>
2021-03-31 12:02:23 +0800

    feat: update helpdesk schema

--- codeine_django/helpdesk/models.py
+++ codeine_django/helpdesk/models.py
@@ -7,6 +7,11 @@ from django.core.exceptions import ValidationError
 import uuid
 
 # Create your models here.
+def img_directory_path(instance, filename):
+    return 'user_{0}/helpdesk/{1}'.format(instance.id, filename)
+# end def
+
+
 class Ticket(models.Model):
     TICKET_STATUSES = (
         ('OPEN', 'Open'),
@@ -15,21 +20,41 @@ class Ticket(models.Model):
     )
 
     TICKET_TYPES = (
-        ('ARTICLE', 'Article'),
-        ('QUERY', 'Query'),
-        ('ENROLLMENT', 'Enrollment')
+        ('ACCOUNT', 'Account'),
+        ('GENERAL', 'General'),
+        ('TECHNICAL', 'Technical'),
+        ('PAYMENT', 'Payments'),
+        ('COURSE', 'Courses'),
+        ('ARTICLE', 'Articles'),
+        ('INDUSTRY_PROJECT', 'Industry Projects'),
+        ('CONSULTATION', 'Consultations'),
     )
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     description = models.CharField(max_length=255)
     timestamp = models.DateTimeField(default=timezone.now)
+    photo = models.ImageField(
+        upload_to=img_directory_path, max_length=100, blank=True, null=True, default=None)
 
     # enums
     ticket_status = models.TextField(choices=TICKET_STATUSES, default='OPEN')
     ticket_type = MultiSelectField(choices=TICKET_TYPES)
 
     # ref
-    base_user = models.ForeignKey('common.BaseUser', on_delete=models.SET_NULL, related_name='tickets', null=True, blank=True)
+    base_user = models.ForeignKey(
+        'common.BaseUser', on_delete=models.SET_NULL, related_name='tickets', null=True, blank=True)
+    transaction = models.ForeignKey(
+        'common.PaymentTransaction', on_delete=models.SET_NULL, related_name='tickets', null=True, blank=True)
+    course = models.ForeignKey(
+        'courses.Course', on_delete=models.SET_NULL, related_name='tickets', null=True, blank=True)
+    article = models.ForeignKey(
+        'community.Article', on_delete=models.SET_NULL, related_name='tickets', null=True, blank=True)
+    industry_project = models.ForeignKey(
+        'industry_projects.IndustryProject', on_delete=models.SET_NULL, related_name='tickets', null=True, blank=True)
+    consultation_slot = models.ForeignKey(
+        'consultations.ConsultationSlot', on_delete=models.SET_NULL, related_name='tickets', null=True, blank=True)
+
 
     def __str__(self):
         return f'Ticket: {self.id}, Status: {self.ticket_status}, Type: {self.ticket_type}'
@@ -37,18 +62,21 @@ class Ticket(models.Model):
 
     class Meta:
         ordering = ['-timestamp', 'ticket_status', 'ticket_type']
-    #end class
+    # end class
 # end class
 
 
 class TicketMessage(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     message = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now)
 
     # ref
-    base_user = models.ForeignKey('common.BaseUser', on_delete=models.SET_NULL, related_name='ticket_messages', null=True, blank=True)
-    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='ticket_messages', null=True, blank=True)
+    base_user = models.ForeignKey('common.BaseUser', on_delete=models.SET_NULL,
+                                  related_name='ticket_messages', null=True, blank=True)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE,
+                               related_name='ticket_messages', null=True, blank=True)
 
     def __str__(self):
         return f'Ticket Message {self.id} for {self.ticket.id} from {self.base_user.id}'
@@ -56,5 +84,5 @@ class TicketMessage(models.Model):
 
     class Meta:
         ordering = ['-timestamp']
-    #end class
+    # end class
 # end class
