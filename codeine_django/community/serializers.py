33a9dee7fa7b348a43862286e6a1c0d15c89eae4
Damien Tan <dtjldamien@gmail.com>
2021-04-04 01:30:34 +0800

    fix: return total comment count for codereview

--- codeine_django/community/serializers.py
+++ codeine_django/community/serializers.py
@@ -285,6 +285,7 @@ class CodeReviewSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField('get_user')
     likes = serializers.SerializerMethodField('get_likes')
     current_user_liked = serializers.SerializerMethodField('get_current_user_liked')
+    num_comments = serializers.SerializerMethodField('get_num_comments')
 
     class Meta:
         model = CodeReview
@@ -315,6 +316,12 @@ class CodeReviewSerializer(serializers.ModelSerializer):
             user = request.user
             return CodeReviewEngagement.objects.filter(code_review=obj).filter(user=user).exists()
         # end if-else
+    # end def
+
+    def get_num_comments(self, obj):
+        request = self.context.get("request")
+        count = CodeReviewComment.objects.filter(code_review=obj).count()
+        return count
     # end def
 # end class
 
