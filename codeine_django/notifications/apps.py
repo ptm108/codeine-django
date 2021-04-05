3af4d7cc32f7c206d490d1a9d5f057c6917bc197
Damien Tan <dtjldamien@gmail.com>
2021-04-06 02:35:08 +0800

    remove scheduler

--- codeine_django/notifications/apps.py
+++ codeine_django/notifications/apps.py
@@ -3,8 +3,4 @@ from django.apps import AppConfig
 
 class NotificationsConfig(AppConfig):
     name = 'notifications'
-
-    def ready(self):
-        import notifications.schedule
-    # end def
 # end class
