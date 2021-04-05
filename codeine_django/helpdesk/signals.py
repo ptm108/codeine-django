123ce6374ec720d5896b5f544c69dcb714d9a646
Damien Tan <dtjldamien@gmail.com>
2021-04-05 11:11:57 +0800

    fix: helpdesk new message notification

--- codeine_django/helpdesk/signals.py
+++ codeine_django/helpdesk/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 from django.db.models import Avg
 
 from .models import TicketMessage
+from common.models import BaseUser
 from courses.models import Course, Enrollment
 from notifications.models import Notification, NotificationObject
 
@@ -14,18 +15,23 @@ def update_ticket_message(sender, instance, created, **kwargs):
     if created:
         sender = instance.base_user
         ticket_owner = instance.ticket.base_user
-        if sender.id != ticket_owner.id:
-            title = f'Helpdesk: New reply for {ticket.description}!'
-            description = f'{instance.message}'
-            notification_type = 'HELPDESK'
-            notification = Notification(
-                title=title, description=description, notification_type=notification_type, ticket=ticket)
-            notification.save()
+        participant_ids = TicketMessage.objects.filter(
+            ticket=instance.ticket).values_list('base_user', flat=True).distinct()
+        title = f'Helpdesk: New reply for {ticket.description}!'
+        description = f'{instance.message}'
+        notification_type = 'HELPDESK'
+        notification = Notification(
+            title=title, description=description, notification_type=notification_type, ticket=ticket)
+        notification.save()
 
-            receiver = ticket.base_user
-            notification_object = NotificationObject(
-                receiver=receiver, notification=notification)
-            notification_object.save()
-        # end if
+        for participant_id in participant_ids:
+            print(participant_id)
+            if sender.id != participant_id:
+                receiver = BaseUser.objects.get(pk=participant_id)
+                notification_object = NotificationObject(
+                    receiver=receiver, notification=notification)
+                notification_object.save()
+            # end if
+        # end for
     # end if
 # end def
