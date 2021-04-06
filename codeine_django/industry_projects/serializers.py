b25f94b7367a3128f42e10b07892b89737b748e2
eliz <e0319168@u.nus.edu>
2021-04-06 23:15:26 +0800

    check if member has applied for IP

--- codeine_django/industry_projects/serializers.py
+++ codeine_django/industry_projects/serializers.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework import serializers
+from common.models import Member
 from common.serializers import NestedBaseUserSerializer
 from .models import (
     IndustryProject, 
@@ -22,6 +24,7 @@ class IndustryProjectApplicationSerializer(serializers.ModelSerializer):
 class IndustryProjectSerializer(serializers.ModelSerializer):
     industry_project_applications = IndustryProjectApplicationSerializer(many=True)
     partner = serializers.SerializerMethodField('get_base_user')
+    is_applied = serializers.SerializerMethodField('get_application')
     class Meta:
         model = IndustryProject
         fields = '__all__'
@@ -30,6 +33,25 @@ class IndustryProjectSerializer(serializers.ModelSerializer):
     def get_base_user(self, obj):
         request = self.context.get("request")
         return NestedBaseUserSerializer(obj.partner.user, context={'request': request}).data
+    # end def
+
+    def get_application(self, obj):
+        request = self.context.get("request")
+        user = request.user
+
+        if (type(user) == AnonymousUser):
+            return None
+        # end if
+
+        member = Member.objects.filter(user=user).first()
+        # print(obj)
+
+        if member is None:
+            return member
+        else:
+            application = IndustryProjectApplication.objects.filter(member=member).filter(industry_project=obj)
+            return application.exists()
+        # end if-else
     # end def
 # end class
 
